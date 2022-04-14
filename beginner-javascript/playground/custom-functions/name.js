f998c39d669cfd377830109bd0ebd9228a19efea
nicklonz <nicklonz@gmail.com>
2022-04-14 00:06:39 -0400

    added stuff

---
+++ beginner-javascript/playground/custom-functions/name.js
@@ -0,0 +1,8 @@
+const firstName = "Nick";
+
+function sayHiTo (firstName) {
+  return `Hello ${firstName}`;
+}
+
+const greeting = sayHiTo("Nick");
+console.log(greeting);
---
+++ beginner-javascript/playground/custom-functions/ways-to-make-a-function.js
@@ -0,0 +1,9 @@
+console.log("It works Nick!!!");
+
+
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+console.log(firstName);
