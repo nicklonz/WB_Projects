616a72cb3e752e196eba4be9a4226bb26a07b226
nicklonz <nicklonz@gmail.com>
2022-04-22 12:20:43 -0400

    backup stuff

--- beginner-javascript/playground/custom-functions/ways-to-make-a-function.js
+++ beginner-javascript/playground/custom-functions/ways-to-make-a-function.js
@@ -1,9 +1,18 @@
 console.log("It works Nick!!!");
+console.log("Wow NL !!!");
 
+console.log(doctorize2("NL"));
+const doctorize = function (firstName) {
+  return `Dr. ${firstName}`;
+};
+function doctorize2 (firstName) {
+  return `Dr. ${firstName}`;
+};
 
+const fruit = ("apple")
 
-function sayHiTo(firstName) {
-  return `Hello ${firstName}`;
-}
+function aIsForApple (fruit) {
+  return `The name of the fruit is ${fruit}!!!`;
+};
 
-console.log(firstName);
+console.log(fruit);
